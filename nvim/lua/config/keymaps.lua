e6be23352acd8b4cf06b93b5103f9db5637f5667
qroske <16343453+qroske@users.noreply.github.com>
2025-03-29 17:28:20 +0900

    Change way comments are written in keymap.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,48 +1,29 @@
 -- 基本的なキーバインドの設定
 local map = vim.api.nvim_set_keymap
 
--- ---------------
--- window
--- ---------------
--- move
+-- window move
 map("n", "<Leader>j", "<C-w>j", { noremap = true, silent = true }) -- ノーマルモードでウィンドウを下に移動
 map("n", "<Leader>k", "<C-w>k", { noremap = true, silent = true }) -- ノーマルモードでウィンドウを上に移動
 map("n", "<Leader>l", "<C-w>l", { noremap = true, silent = true }) -- ノーマルモードでウィンドウを右に移動
 map("n", "<Leader>h", "<C-w>h", { noremap = true, silent = true }) -- ノーマルモードでウィンドウを左に移動
-
--- split
+-- window split
 map("n", "<Leader>s", ":<C-u>sp\n", { noremap = true }) -- ノーマルモードでウィンドウの水平分割
 map("n", "<Leader>v", ":<C-u>vs\n", { noremap = true }) -- ノーマルモードでウィンドウの垂直分割
--- close
+-- window close
 map("n", "<Leader>w", ":<C-u>w\n", { noremap = true }) -- ノーマルモードで現在のウィンドウを保存する
 map("n", "<Leader>wq", ":<C-u>wq\n", { noremap = true }) -- ノーマルモードで現在のウィンドウを保存して閉じる
 map("n", "<Leader>q", ":<C-u>q\n", { noremap = true }) -- ノーマルモードで現在のウィンドウを閉じる
 map("n", "<Leader>q!", ":<C-u>q!\n", { noremap = true }) -- ノーマルモードで現在のウィンドウを強制的に閉じる
-
--- ---------------
--- cursor
--- ---------------
--- move
+-- cursor move
 map("i", "<C-j>", "<Down>", {}) -- インサートモードでカーソルを下に移動
 map("i", "<C-k>", "<Up>", {}) -- インサートモードでカーソルを上に移動
 map("i", "<C-l>", "<Right>", {}) -- インサートモードでカーソルを右に移動
 map("i", "<C-h>", "<Left>", {}) -- インサートモードでカーソルを左に移動
-
--- ---------------
--- terminal
--- ---------------
--- open
+-- terminal open
 map("n", "<Leader>tt", ":terminal\n", { noremap = true }) -- ノーマルモードでターミナルウィンドウを開く
-
--- ---------------
--- diagnostics
--- ---------------
--- show
+-- diagnostics show
 map("n", "<Leader>d", ":lua vim.diagnostic.open_float()<CR>", { noremap = true }) -- ノーマルモードでエラーや警告を表示する
-
--- ---------------
 -- project
--- ---------------
 map("n", "<Leader>p", ":Telescope project", { noremap = true, silent = true })
 -- Ctrl-c to esc
 map("i", "<C-c>", "<Esc>", { noremap = true, silent = true })
