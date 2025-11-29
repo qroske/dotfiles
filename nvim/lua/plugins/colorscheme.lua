61c7561b0f31bf5fe72df22701be257d7eff1402
qroske <16343453+qroske@users.noreply.github.com>
2025-11-29 17:38:38 +0900

    add tokyonight as colorscheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+-- --------------------------------
+-- ColorScheme設定
+-- --------------------------------
+return {
+  -- TokyoNight カラースキーム
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- Neovim起動時にすぐにロードする
+    priority = 1000, -- 他のプラグインより先にロードされるように優先度を高く設定
+    -- オプション設定
+    opts = {
+      style = "night", -- テーマのスタイルを 'night' に設定
+    },
+    -- プラグインロード後の設定
+    config = function()
+      -- Neovimが起動したら、カラースキームを 'tokyonight' に設定
+      vim.cmd("colorscheme tokyonight")
+    end
+  },
+}
