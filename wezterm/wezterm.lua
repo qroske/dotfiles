7b75d0933277c04f40cd6ffdf7be3db00ae6ff35
qroske <16343453+qroske@users.noreply.github.com>
2025-12-30 14:24:45 +0900

    change settings for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,17 +11,30 @@ config.automatically_reload_config = true
 -- カラースキームの設定
 config.color_scheme = "Tokyo Night"
 -- フォント設定
-config.font_size = 16
+config.font_size = 15
 -- IMEで日本語入力をできるようにする
 config.use_ime = true
 -- 背景透過
 config.window_background_opacity = 0.85
 -- 背景にぼかしを追加
--- config.macos_window_background_blur = 20
+config.macos_window_background_blur = 20
 -- タイトルバーの削除
 config.window_decorations = "RESIZE"
+-- タブが一つしかない時に非表示
+config.hide_tab_bar_if_only_one_tab = false
+-- タブバーを透明にする
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
+-- タブバーを背景と同じ色（黒）にする
+config.window_background_gradient = {
+  colors = { "#000000" },
+}
 -- タブの追加ボタンを非表示
 config.show_new_tab_button_in_tab_bar = false
+-- タブの閉じるボタンを非表示(nightlyでのみ有効)
+-- config.show_close_tab_button_in_tabs = false
 -- タブ同士の境界線を非表示
 config.colors = {
   tab_bar = {
@@ -34,16 +47,62 @@ config.clean_exit_codes = {}
 ---------------------------------------
 -- keybinds
 ---------------------------------------
+-- デフォルトのキーバインディングを無効にする
 config.disable_default_key_bindings = false
+-- keybuinds.luaを読み込めるようにする
 config.keys = require("keybinds").keys
+-- Leader Key
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
 ---------------------------------------
 -- customize
 ---------------------------------------
+-- アクティブタブに色をつける
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+local WORKSPACE_ICON = wezterm.nerdfonts.oct_terminal
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#5c6d74"
+  local foreground = "#FFFFFF"
+  local edge_background = "none"
+
+  if tab.is_active then
+    background = "#4169e1"
+    foreground = "#FFFFFF"
+  end
+
+  local edge_foreground = background
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
 -- Workspace名を左上に表示
 wezterm.on("update-right-status", function(window, _)
   local workspace = window:active_workspace()
-  window:set_left_status(wezterm.format({ { Text = workspace } }))
+  local background = "none"
+  local foreground = "#FFFFFF"
+
+  window:set_left_status(wezterm.format({
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = " " .. WORKSPACE_ICON .. " " },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = workspace },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = "" },
+  }))
 end)
 
 return config
