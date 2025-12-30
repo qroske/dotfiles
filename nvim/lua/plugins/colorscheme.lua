1d5463c3d77e814a832bbda7fb86509dfb410632
qroske <16343453+qroske@users.noreply.github.com>
2025-12-31 03:07:15 +0900

    make neovim background transparent

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -10,9 +10,15 @@ return {
     -- オプション設定
     opts = {
       style = "night", -- テーマのスタイルを 'night' に設定
+      transparent = true, -- 透過を有効化
+      styles = {
+        sidebars = "transparent", -- Neo-treeなどのサイドバーを透過
+        floats = "transparent", -- 浮き出しウィンドウを透過
+      }
     },
     -- プラグインロード後の設定
-    config = function()
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
       -- Neovimが起動したら、カラースキームを 'tokyonight' に設定
       vim.cmd("colorscheme tokyonight")
     end
